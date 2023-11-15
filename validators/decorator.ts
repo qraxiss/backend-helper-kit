cc6838288758999f127d604b0d57342d7ad6572d
qraxiss <qraxiss@gmail.com>
2023-11-15 15:35:20 +0300

    if validator dosent exist, return Joi.any()

--- validators/decorator.ts
+++ validators/decorator.ts
@@ -1,18 +1,23 @@
 import { validate } from './validate'
 
+import Joi from 'joi'
+
 export function avalidator(inputTypes: any, outputTypes: any, config: any) {
     return function (target: any, name: string, descriptor: any) {
         const originalFunction = descriptor.value
 
+        var inputValidator = (inputTypes as any)[name] || Joi.any()
+        var outputValidator = (outputTypes as any)[name] || Joi.any()
+
         if (config.ENV === 'development') {
             descriptor.value = async function (params: any) {
-                let value = validate(params, (inputTypes as any)[name])
+                let value = validate(params, inputValidator)
                 const result = await originalFunction.call(this, value)
-                return validate(result, (outputTypes as any)[name])
+                return validate(result, outputValidator)
             }
         } else if (config.ENV === 'production') {
             descriptor.value = async function (params: any) {
-                let value = validate(params, (inputTypes as any)[name])
+                let value = validate(params, inputValidator)
                 return await originalFunction.call(this, value)
             }
         }
